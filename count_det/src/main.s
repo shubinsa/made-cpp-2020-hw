d9bb937b3a2e59d862af5ef969a53fc8f446a81d
shubinsa <shubinsa1@gmail.com>
2020-09-29 13:28:32 +0300

    dz_2_done

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,37 @@
-/// Your code goes here...
-
+.text
+.global count_det
+count_det:
+	ldr r1, [r0], #4
+	ldr r2, [r0], #4
+	ldr r3, [r0], #4
+	ldr r4, [r0], #4
+	ldr r5, [r0], #4
+	ldr r6, [r0], #4
+	ldr r7, [r0], #4
+	ldr r8, [r0], #4
+	ldr r9, [r0], #4
+	mul r10, r5, r9 
+	mul r11, r6, r8
+	sub r12, r10, r11
+	mul r1, r12
+	mul r10, r4, r9 
+	mul r11, r6, r7
+	sub r12, r10, r11
+	mul r2, r12
+	mul r10, r4, r8 
+	mul r11, r5, r7
+	sub r12, r10, r11
+	mul r3, r12
+	sub r1, r2
+	add r1, r3
+	mov r0, r1
+	pop {r4}
+	pop {r5}
+	pop {r6}
+	pop {r7}
+	pop {r8}
+	pop {r9}
+	pop {r10}
+	pop {r11}
+	pop {r12}
+	bx lr
